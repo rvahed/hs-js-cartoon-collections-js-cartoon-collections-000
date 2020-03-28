6bc361576eb83436bcbe132e19c50898b5655e3c
Rehan Vahed <rvahed@hotmail.co.uk>
2020-03-28 16:32:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 function dwarfRollCall(dwarves) {
+var call = []
+  dwarves.forEach(function(entry, i){
+    call.push(i+1 + ". " + entry+" ")
+  })
+  return call.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var element = []
+planeteerCalls.forEach(function(word){
+  word = word.toUpperCase() + "!"
+  element.push(word)
+})
+return element
 }
 
 function longPlaneteerCalls(words) {
+for (let i =0; i < words.length; i++){
+    if(words.length > 4){
+      return true
+    }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  var cheese = ""
+  let cheddar="cheddar", gouda= "gouda", camembert="camembert"
+  for(var i = 0; i<foods.length; i++){
+    cheese = foods[i]
+    if(cheese===cheddar){
+      return(`${cheese}`)
+    }
+    if(cheese===gouda){
+      return(`${cheese}`)
+    }
+    if(cheese===camembert){
+      return(`${cheese}`)
+    }
+  }return("no cheese!")
 }
